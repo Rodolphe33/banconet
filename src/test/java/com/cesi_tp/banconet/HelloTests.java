48eabc0cd84fd97253339df3a0447153cf012684
rodolphe.lassalle33 <rodolphe.lassalle33@gmail.com>
2020-10-15 14:06:26 +0200

    Ajout d'un fichier test

---
+++ src/test/java/com/cesi_tp/banconet/HelloTests.java
@@ -0,0 +1,12 @@
+package com.cesi_tp.banconet;
+
+import org.juint.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTests {
+    @Test
+    void contextLoads(){
+        
+    }
+}
