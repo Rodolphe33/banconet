099ce42adc64397b6dce1b81d12abbbdc0f9b70e
rodolphe.lassalle33 <rodolphe.lassalle33@gmail.com>
2020-10-15 11:21:39 +0200

    premier commit

---
+++ src/test/java/com/cesi_tp/banconet/BanconetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesi_tp.banconet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanconetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
